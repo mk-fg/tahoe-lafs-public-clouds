f3d7385ab28db0c143f9d8c34bed20570ba9340f
Mike Kazantsev <mk.fraggod@gmail.com>
2012-10-06 19:50:25 +0600

    skydrive: added caching of folder list and batching of concurrent calls

--- src/allmydata/storage/backends/cloud/skydrive/skydrive_container.py
+++ src/allmydata/storage/backends/cloud/skydrive/skydrive_container.py
@@ -3,7 +3,7 @@ from os.path import basename, dirname, normpath, join
 import re
 
 from zope.interface import implements
-from twisted.internet import defer
+from twisted.internet import reactor, defer
 from twisted.web import http
 
 from allmydata.node import InvalidValueError, MissingConfigEntry
@@ -86,6 +86,61 @@ def decode_object_name(name_enc):
         name_enc = name_enc.encode('utf-8')
     return '__'.join(c.replace('_', '/') for c in name_enc.split('__'))
 
+
+class DeferredCache(object):
+
+    _empty = object()
+    cache = _empty
+    cache_ttl_flush = None
+    callback = None
+    deferred = None
+
+    def __init__(self, cache_ttl=None):
+        self.listeners = set()
+        self.cache_ttl = cache_ttl
+
+    def _activate(self, res):
+        assert self.deferred
+        self.deferred = None
+        for d in self.listeners: d.callback(res)
+        if self.cache_ttl is not None:
+            self.cache = res
+            if self.cache_ttl > 0:
+                self.cache_ttl_flush = reactor.callLater(self.cache_ttl, self.flush)
+        self.listeners.clear()
+        return res
+
+    def register_update_callback(self, func, *args, **kwargs):
+        self.callback = func, args, kwargs
+
+    def register_deferred(self, d=None):
+        if self.cache is not self._empty:
+            if not d: d = defer.succeed(self.cache)
+            else: d.callback(self.cache)
+            return d
+        if not self.deferred: self.refresh()
+        if not d: d = defer.Deferred()
+        self.listeners.add(d)
+        return d
+
+    def refresh(self):
+        if self.deferred: # already in progress
+            return self.deferred
+        self.flush()
+        func, args, kwargs = self.callback
+        d = self.deferred = func(*args, **kwargs)
+        d.addBoth(self._activate)
+        return d
+
+    def flush(self, ignored=None):
+        self.cache = self._empty
+        if self.cache_ttl_flush:
+            if self.cache_ttl_flush.active():
+                self.cache_ttl_flush.cancel()
+            self.cache_ttl_flush = None
+        return ignored
+
+
 class SkyDriveItem(object):
 
     def __init__(self, key, modification_date, etag, size, storage_class, owner=None):
@@ -162,39 +217,56 @@ class SkyDriveContainer(ContainerRetryMixin):
         def _create_folder(parent_info, name):
             return self._do_request('mkdir', self.client.mkdir, name, parent_info['id'])
 
-        def _folder_lookup_error(failure, path):
+        def _folder_lookup_error(failure):
             failure.trap(DoesNotExists)
-            parent, missing_component = failure.value.args
-            path = path.rsplit('/', 1)
-            if len(path) > 1:
-                d = self._do_request('resolve path', self.client.resolve_path, path[0])
-                d.addCallback(_create_folder, path[-1])
-                d.addErrback(_folder_lookup_error, path[0]) # try to create parent path
-                d.addCallback(_create_folder, path[-1]) # retry
-            else:
-                d = self._do_request('mkdir', self.client.mkdir, path[-1])
+            parent_id, slugs = failure.value.args
+            d = _create_folder(dict(id=parent_id), slugs[0])
+            for slug in slugs[1:]:
+                d.addCallback(_create_folder, slug)
             return d
 
-        def _flush_idmap(ignored):
+        def _flush_idmap(failure):
+            failure.trap(DoesNotExists)
             self.chunk_idmap = anydbm.open(self.chunk_idmap_path, 'n')
-            return ignored
+            return failure
 
         d.addCallback(_match_folder_id)
         d.addErrback(_flush_idmap)
-        d.addErrback(_folder_lookup_error, folder_path)
+        d.addErrback(_folder_lookup_error)
         d.addCallback(_match_folder_id)
         return d
 
 
+    _create_combinator_v = None
+    @property
+    def _create_combinator(self):
+        if not self._create_combinator_v:
+            self._create_combinator_v = DeferredCache()
+            self._create_combinator_v.register_update_callback(
+                self._do_request, 'create/check folder',
+                self.ensure_folder, self.folder_path, self.folder_id )
+        return self._create_combinator_v
+
+    _list_cache_v = None
+    _list_cache_ttl = 120
+    @property
+    def _list_cache(self):
+        if not self._list_cache_v:
+            self._list_cache_v = DeferredCache(cache_ttl=self._list_cache_ttl)
+            self._list_cache_v.register_update_callback(
+                self._do_request, 'list objects', self.client.listdir, self.folder_id, type_filter='file' )
+        return self._list_cache_v
+
+
     def create(self):
-        return self._do_request( 'create/check folder',
-            self.ensure_folder, self.folder_path, self.folder_id )
+        return self._create_combinator.register_deferred()
 
     def delete(self):
         d = self._do_request('delete folder', self.client.delete, self.folder_id)
         def _flush_idmap(ignored):
             self.chunk_idmap = anydbm.open(self.chunk_idmap_path, 'n')
         d.addCallback(_flush_idmap)
+        d.addCallback(self._list_cache.flush)
         return d
 
 
@@ -205,6 +277,7 @@ class SkyDriveContainer(ContainerRetryMixin):
             failure.trap(ProtocolError)
             if failure.value.code not in [http.NOT_FOUND, http.GONE]:
                 return failure
+            self._list_cache.flush()
             d = self.create()
             d.addCallback(lambda ignored: func(self, *args, **kwargs))
             return d
@@ -224,7 +297,6 @@ class SkyDriveContainer(ContainerRetryMixin):
         try:
             return defer.succeed(self.chunk_idmap[object_name])
         except KeyError: pass
-
         d = self._do_request( 'resolve object path', self.client.resolve_path,
             encode_object_name(object_name), root_id=self.folder_id )
         def _update_idmap(cid):
@@ -236,7 +308,7 @@ class SkyDriveContainer(ContainerRetryMixin):
 
     @handle_enoent
     def list_objects(self, prefix=''):
-        d = self._do_request('list objects', self.client.listdir, self.folder_id, type_filter='file')
+        d = self._list_cache.register_deferred()
         def _filter_objects(lst):
             contents = list()
             for info in lst:
@@ -258,6 +330,7 @@ class SkyDriveContainer(ContainerRetryMixin):
         def _cache_object_id(info):
             self.chunk_idmap[object_name] = info['id']
         d.addCallback(_cache_object_id)
+        d.addCallback(self._list_cache.flush)
         return d
 
     def get_object(self, object_name):
@@ -274,4 +347,5 @@ class SkyDriveContainer(ContainerRetryMixin):
     def delete_object(self, object_name):
         d = self.resolve_object_name(object_name)
         d.addCallback(lambda cid: self._do_request('DELETE object', self.client.delete, cid))
+        d.addCallback(self._list_cache.flush)
         return d
