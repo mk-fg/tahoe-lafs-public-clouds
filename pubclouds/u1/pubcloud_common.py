ce80cf9e87f3e2c2d31f5869d3b52b4414e70f7c
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-15 20:19:05 +0600

    u1: use pubcloud_common where possible

---
+++ pubclouds/u1/pubcloud_common.py
@@ -0,0 +1,1 @@
+../pubcloud_common.py
--- pubclouds/u1/u1_container.py
+++ pubclouds/u1/u1_container.py
@@ -2,17 +2,16 @@
 
 import itertools as it, operator as op, functools as ft
 from os.path import dirname, basename
-from time import time
 
-from zope.interface import implements
 from twisted.internet import reactor, defer
 
 from allmydata.node import InvalidValueError, MissingConfigEntry
-from allmydata.storage.backends.cloud.cloud_common import (
-	IContainer, ContainerRetryMixin, CloudError,
-	CloudServiceError, ContainerItem, ContainerListing,
-	CommonContainerMixin, HTTPClientMixin )
 from allmydata.util import log
+
+from .pubcloud_common import (
+	encode_key, decode_key,
+	PubCloudItem, PubCloudListing, PubCloudContainer )
+
 
 
 def configure_u1_container(storedir, config):
@@ -65,95 +64,6 @@ def configure_u1_container(storedir, config):
 
 
 
-def token_bucket(interval, burst=1, borrow=True):
-	tokens, rate, ts_sync = burst, interval**-1, time()
-	val = yield
-	while True:
-		if val is None: val = 1
-		ts = time()
-		ts_sync, tokens = ts, min(burst, tokens + (ts - ts_sync) * rate)
-		if tokens >= val: delay, tokens = None, tokens - val
-		else:
-			delay = (val - tokens) / rate
-			if borrow: tokens -= val
-		val = yield delay
-
-class ContainerRateLimitMixin(object):
-	# Uses/expects container._reactor attribute set for the object
-	# TODO: generic IMixin with fixed _do_request method
-
-	rate_errors = 420,
-	retry_backoff = 60, 120, 120, 600
-
-	def __init__(self, interval, burst):
-		if interval <= 0 or burst <= 0: # no limit
-			self.bucket = None
-			return
-		self.bucket = token_bucket(interval, burst)
-		next(self.bucket)
-
-	def _delay(self, seconds):
-		d = defer.Deferred()
-		self._reactor.callLater(seconds, d.callback, None)
-		return d
-
-	@defer.inlineCallbacks
-	def _do_request(self, *argz, **kwz):
-		func = ft.partial( self._rate_limit_retries,
-			super(ContainerRateLimitMixin, self)._do_request, *argz, **kwz )
-		if not self.bucket: defer.returnValue((yield func()))
-		delay = next(self.bucket)
-		if delay is None: defer.returnValue((yield func()))
-		yield self._delay(delay)
-		defer.returnValue((yield func()))
-
-	@defer.inlineCallbacks
-	def _rate_limit_retries(self, func, *argz, **kwz):
-		# These are bound to happen with free-API's, unfortunately
-		try: defer.returnValue((yield func(*argz, **kwz)))
-		except CloudError as err:
-			if err.args[1] not in self.rate_errors: raise
-			log.msg( 'Got rate-limit error from the API ({} {}), retrying after'
-				' a delay'.format(err.args[1], err.args[2]), level=log.OPERATIONAL )
-		# Do it the hard way
-		for attempt, delay in enumerate(self.retry_backoff, 1):
-			yield self._delay(delay)
-			try: defer.returnValue((yield func(*argz, **kwz)))
-			except CloudError as err:
-				if err.args[1] not in self.rate_errors\
-					or attempt == len(self.retry_backoff): raise
-
-
-
-class U1Item(ContainerItem):
-	# 'key', 'modification_date', 'etag', 'size', 'storage_class', 'owner'
-
-	backend_id = None
-	storage_class = 'STANDARD'
-
-	def __init__(self, info, **kwz):
-		self.backend_id = kwz.pop('backend_id', None) or info['content_path']
-		super(U1Item, self).__init__(
-			kwz.pop('key'),
-			kwz.pop('modification_date', None) or info['when_changed'], # modification_date
-			self.backend_id, # etag
-			kwz.pop('size', None) or info['size'], # size
-			self.storage_class ) # storage_class
-		for k, v in kwz.viewitems(): setattr(self, k, v) # extras
-
-
-class U1Listing(ContainerListing):
-	# 'name', 'prefix', 'marker', 'max_keys', 'is_truncated', 'contents', 'common_prefixes'
-
-	max_keys = 2**30
-	is_truncated = 'false'
-
-	def __init__(self, name, prefix, contents):
-		super(U1Listing, self).__init__(
-			name, prefix, None, self.max_keys, self.is_truncated, contents=contents )
-
-
-
 def join(*path_nodes):
 	path = ''
 	for node in path_nodes:
@@ -163,36 +73,48 @@ def join(*path_nodes):
 	return path
 
 
+class U1Item(PubCloudItem):
 
-class U1Container(ContainerRateLimitMixin, ContainerRetryMixin):
-	implements(IContainer)
+	modification_date_key = 'when_changed'
+
+	def __init__(self, info, **kwz):
+		assert 'key' in kwz, kwz
+		info['id'] = info['content_path']
+		super(U1Item, self).__init__(info, **kwz)
+
+class U1Listing(PubCloudListing): pass
+
+
+class U1Container(PubCloudContainer):
 
 	def __init__( self, api, data_path,
 			auth_consumer, auth_token,
 			override_reactor=None ):
-		from txu1.api_v1 import txU1, ProtocolError, DoesNotExist
+		from txu1.api_v1 import txU1, ProtocolError, DoesNotExists
 
 		self.client = txU1(
 			auth_consumer=auth_consumer, auth_token=auth_token,
 			debug_requests=api['debug'], request_io_timeouts=api['timeouts'] )
 		self.data_path = data_path
 
-		self._reactor = override_reactor or reactor
-
-		self.ProtocolError, self.DoesNotExist = ProtocolError, DoesNotExist
+		self.ProtocolError, self.DoesNotExists = ProtocolError, DoesNotExists
 		self.ServiceError = ProtocolError
+		self.build_item, self.build_listing = U1Item, U1Listing
+
 		super(U1Container, self).__init__(
-			interval=api['tb_interval'], burst=api['tb_burst'] )
+			tb_interval=api['tb_interval'], tb_burst=api['tb_burst'],
+			override_reactor=override_reactor )
 
 		self._listdir_cache = None # to be filled
 		self._listdir_cache_dirs = None # used on put requests
-		self._listdir_cache_lock = defer.DeferredLock()
 
 	def __repr__(self):
 		return '<{} {!r}>'.format(self.__class__.__name__, self.data_path)
 
 
-	def create(self): pass # paths are auto-created here
+	def _mkdir(self): pass # paths are auto-created here
+	def _rmdir(self): raise NotImplementedError('Should not be used in U1 driver')
+	def _chunks_flush(self): pass # custom caches here
 
 	def delete(self):
 		return self._do_request('delete root', self.client.node_delete, self.data_path)
@@ -203,8 +125,10 @@ class U1Container(ContainerRateLimitMixin, ContainerRetryMixin):
 		if top:
 			self._listdir_cache = dict()
 			self._listdir_cache_dirs = dict()
-		try: info = yield self._do_request('listdir', self.client.node_info, path, children=True)
-		except self.DoesNotExist:
+		try:
+			info = yield self._do_request( 'listdir',
+				self.err503_wrapper, self.client.node_info, path, children=True )
+		except self.DoesNotExists:
 			if not top: raise
 			defer.returnValue(None) # will be created
 
@@ -213,7 +137,7 @@ class U1Container(ContainerRateLimitMixin, ContainerRetryMixin):
 			if node['kind'] == 'file':
 				assert node['resource_path'].startswith(self.data_path)
 				key = node['resource_path'][dp_len:].lstrip('/')
-				self._listdir_cache[key] = U1Item(node, key=key)
+				self._listdir_cache[key] = self.build_item(node, key=key)
 			elif node['kind'] == 'directory':
 				self._listdir_cache_dirs[node['resource_path']] = node['content_path']
 				deferreds.append(self._listdir_cache_build(node['resource_path']))
@@ -222,13 +146,13 @@ class U1Container(ContainerRateLimitMixin, ContainerRetryMixin):
 
 	@defer.inlineCallbacks
 	def list_objects(self, prefix=''):
-		yield self._listdir_cache_lock.acquire()
+		yield self._chunks_lock.acquire()
 		try:
 			if self._listdir_cache is None:
 				yield self._listdir_cache_build(self.data_path, top=True)
-		finally: self._listdir_cache_lock.release()
+		finally: self._chunks_lock.release()
 		defer.returnValue(
-			U1Listing(self.data_path, prefix, list(
+			self.build_listing(self.data_path, prefix, list(
 				item for key, item in self._listdir_cache.viewitems()
 				if not prefix or key.startswith(prefix) )) )
 
@@ -250,7 +174,7 @@ class U1Container(ContainerRateLimitMixin, ContainerRetryMixin):
 		node = yield self._do_request( 'put',
 			self.client.file_put_into, content_path=dst, name=path_file_name, data=data )
 		key = node['resource_path'][len(self.data_path):].lstrip('/')
-		self._listdir_cache[key] = U1Item(node, key=key)
+		self._listdir_cache[key] = self.build_item(node, key=key)
 
 	@defer.inlineCallbacks
 	def delete_object(self, key):
